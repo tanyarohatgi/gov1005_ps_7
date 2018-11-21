c21aa11f93dbb2cff9d6e01a10b38d9e5bf3e389
Tanya Rohatgi <tanyarohatgi@Tanyas-MacBook-Pro-New.local>
2018-11-20 22:13:21 -0500

    Reactive plot done. Add line of best fit, other details.

--- midterm_analysis/app.R
+++ midterm_analysis/app.R
@@ -7,42 +7,65 @@
 #    http://shiny.rstudio.com/
 #
 
+library(tidyverse)
 library(shiny)
+library(ggplot2)
+
+data <- read_rds("graph1.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  
+  # Application title
+  titlePanel("Political Landscape Survey, Summer 2017"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "x",
+        label = "X-Axis: Select Vote Shares from Recent Elections:",
+        choices = c("Clinton, 2016" = "clinton16", "Trump, 2016" = "trump16", "Democrats in the House, 2016" = "demhouse16", "Republicans in the House, 2016" = "rephouse16", 
+                    "Obama, 2012" = "obama12",
+                    "Romney, 2012" = "romney12"),
+      )),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("scatterPlot")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+server <- function(input, output) { 
+  
+  
+  output$scatterPlot <- renderPlot({
+    
+    x <- reactive({ 
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+      str_to_title(str_replace_all(input$x, c("clinton16" = "Clinton Vote Share in 2016", 
+                                             "trump16" = "Trump Vote Share in 2016",
+                                             "demhouse16" = "House Democrats Vote Share in 2016",
+                                             "rephouse16" = "House Republicans Vote Share in 2016",
+                                             "obama12" = "Obama Vote Share in 2012",
+                                             "romney12" = "Romney Vote Share in 2012"))) 
+      
+      })
+    
+    
+    
+    data %>%
+      ggplot(aes_string(x = input$x, y = "error", color = "state")) + 
+      geom_jitter() + xlab(x()) + ylab("Polling Error (in %)") + 
+      ggtitle("Placeholder:") + 
+      labs(subtitle = "Placeholder") + 
+      theme_minimal() + labs(color = "State")
+    
+    
+    
+  })
 }
 
 # Run the application 
